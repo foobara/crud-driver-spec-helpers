1af09dd7f4d48ccac3a9c427b1e7d31605be591e
Miles Georgi <azimux@gmail.com>
2025-06-08 16:51:09 -0700

    Create ruby project files

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## [Unreleased]
+
+- Move version.rb.erb to top-level
+
+## [0.0.1] - 2025-06-08
+
+- Initial release
---
+++ spec/foobara/crud_driver_spec_helpers_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Foobara::CrudDriverSpecHelpers do
+  it "has a version number" do
+    expect(Foobara::CrudDriverSpecHelpers::VERSION).to_not be_nil
+  end
+end
